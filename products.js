66c596f3a2688cb1afb62feb437836ec4e3ad5a2
Nihad <nihadalizadaxx@gmail.com>
2023-08-19 20:16:11 +0400

    react online shop with redux

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12pro_e09phn.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.apple.com%2Fuk%2Fshop%2Fproduct%2FFJNP3B%2FA%2Frefurbished-iphone-12-128gb-purple-sim-free&psig=AOvVaw0biT8klhdGftECZVmHLYFn&ust=1692357899436000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNDOt83K44ADFQAAAAAdAAAAABAZ",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+];
+
+module.exports = products;
